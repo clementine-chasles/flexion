c4f53ed1023c35014bec93707089507a23adf93a
clementine-chasles <clementine.chasles@gmail.com>
2015-11-15 15:49:07 +0100

    Adding javadoc comments to MyIntegration

--- src/main/java/flexion/integrationteam/domain/MyIntegration.java
+++ src/main/java/flexion/integrationteam/domain/MyIntegration.java
@@ -12,20 +12,39 @@ import flexion.integrationteam.handler.FlexionHandler;
 
 @Component
 public class MyIntegration implements Integration {
-	
+
 	@Autowired
 	private FlexionHandler flexionHandler;
-	
-	public MyIntegration() { }
 
+	public MyIntegration() {
+	}
+
+	/**
+	 * Performs a purchase
+	 * 
+	 * @param item
+	 *            id of the item to be bought
+	 * @return the purchase
+	 */
 	public Purchase buy(String item) {
 		return flexionHandler.buy(item);
 	}
 
+	/**
+	 * Sets the status of a purchase to <i>consumed</i>
+	 * 
+	 * @param purchase
+	 *            the purchase that needs to be consumed
+	 */
 	public void consume(Purchase purchase) {
 		flexionHandler.consume(purchase);
 	}
 
+	/**
+	 * returns a history of previous purchases and their consumption statuses
+	 * 
+	 * @return a list of purchases
+	 */
 	public List<Purchase> getPurchases() {
 		return flexionHandler.getPurchases();
 	}
